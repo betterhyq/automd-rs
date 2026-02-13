37d1c28231d2f474b1ce3b110ce747b83be869dd
heyongqi10 <heyongqi10@jd.com>
2026-02-13 17:34:12 +0800

    chore: update dependencies in Cargo.lock and Cargo.toml, add logging capabilities with env_logger, and enhance main function for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 //! CLI: run from current dir (Cargo.toml + README.md).
 
 use std::path::Path;
+use log::{info, warn};
 
 fn main() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let manifest_dir = Path::new(".");
     let readme_path = Path::new("README.md");
+    info!("manifest_dir: {:?}", manifest_dir);
+    info!("readme_path: {:?}", readme_path);
     if let Err(e) = automd_rs::run(manifest_dir, readme_path) {
-        eprintln!("error: {}", e);
+        warn!("error: {}", e);
         std::process::exit(1);
     }
 }
