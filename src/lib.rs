38233baef65b32b2f7d4f049104ad83dd08ea4bf
heyongqi10 <heyongqi10@jd.com>
2026-02-13 16:09:37 +0800

    feat: add c12-parser and serde dependencies; implement Cargo.toml parsing functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
-pub fn add_two_numbers(a: i32, b: i32) -> i32 {
-    a + b
+use find_cargo_toml::find_from_current_dir;
+use std::fs;
+use c12_parser::{parse_toml, FormatOptions, Formatted};
+use serde::Deserialize;
+
+/// Minimal Cargo.toml shape: only need [package] name.
+#[derive(Debug, Deserialize)]
+struct CargoToml {
+    package: Package,
+}
+
+#[derive(Debug, Deserialize)]
+struct Package {
+    name: String,
+}
+
+fn parse_cargo_toml() -> String {
+    let cargo_toml = find_from_current_dir(".", None).next().unwrap();
+    let cargo_toml_content = fs::read_to_string(cargo_toml).unwrap();
+    let toml: Formatted<CargoToml> =
+        parse_toml(&cargo_toml_content, Some(FormatOptions::default())).unwrap();
+    let name = &toml.value.package.name;
+    return name.to_string();
 }
 
 #[cfg(test)]
@@ -7,7 +28,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add_two_numbers() {
-        assert_eq!(add_two_numbers(1, 2), 3);
+    fn test_parse_cargo_toml() {
+        let name = parse_cargo_toml();
+        assert_eq!(name, "automd-rs");
     }
 }
