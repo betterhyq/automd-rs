d44e7479eefb8a7f98b0e577a8a1b31e6466f015
heyongqi10 <heyongqi10@jd.com>
2026-02-13 16:19:29 +0800

    feat: add main function to generate and print badges for Crates.io and GitHub metrics

--- src/badges.rs
+++ src/badges.rs
@@ -1,19 +1,19 @@
 pub fn generate_crate_version_badge(name: &str) -> String {
-    return format!("![Crates.io Version](https://img.shields.io/crates/v/${name})");
+    return format!("![Crates.io Version](https://img.shields.io/crates/v/{name})");
 }
 
 pub fn generate_crate_downloads_badge(name: &str) -> String {
-    return format!("![Crates.io Total Downloads](https://img.shields.io/crates/d/${name})");
+    return format!("![Crates.io Total Downloads](https://img.shields.io/crates/d/{name})");
 }
 
 pub fn generate_crate_docs_badge(name: &str) -> String {
-    return format!("![docs.rs](https://img.shields.io/docsrs/${name})");
+    return format!("![docs.rs](https://img.shields.io/docsrs/{name})");
 }
 
 pub fn generate_commit_activity_badge(username: &str, repository: &str) -> String {
-    return format!("![GitHub commit activity](https://img.shields.io/github/commit-activity/m/${username}/${repository})");
+    return format!("![GitHub commit activity](https://img.shields.io/github/commit-activity/m/{username}/{repository})");
 }
 
 pub fn generate_repo_stars_badge(username: &str, repository: &str) -> String {
-    return format!("![GitHub Repo stars](https://img.shields.io/github/stars/${username}/${repository})");
+    return format!("![GitHub Repo stars](https://img.shields.io/github/stars/{username}/{repository})");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod badges;
+
 use find_cargo_toml::find_from_current_dir;
 use std::fs;
 use c12_parser::{parse_toml, FormatOptions, Formatted};
 use serde::Deserialize;
+use url::Url;
 
 /// Minimal Cargo.toml shape: only need [package] name.
 #[derive(Debug, Deserialize)]
@@ -23,6 +26,32 @@ fn parse_cargo_toml() -> (String, String) {
     let name = &toml.value.package.name;
     let repository = &toml.value.package.repository;
     return (name.to_string(), repository.to_string());
+}
+
+fn parse_github_repository_url(repository: &str) -> (String, String) {
+    let url = Url::parse(repository).unwrap();
+    let path = url.path();
+    let parts = path.split('/').collect::<Vec<&str>>();
+    let username = parts[1];
+    let repository = parts[2];
+    return (username.to_string(), repository.to_string());
+}
+
+fn main() {
+    let (name, repository) = parse_cargo_toml();
+    let version_badge = badges::generate_crate_version_badge(&name);
+    let downloads_badge = badges::generate_crate_downloads_badge(&name);
+    let docs_badge = badges::generate_crate_docs_badge(&name);
+
+    let (username, repository_name) = parse_github_repository_url(&repository);
+
+    let commit_activity_badge = badges::generate_commit_activity_badge(&username, &repository_name);
+    let repo_stars_badge = badges::generate_repo_stars_badge(&username, &repository_name);
+    println!("{}", version_badge);
+    println!("{}", downloads_badge);
+    println!("{}", docs_badge);
+    println!("{}", commit_activity_badge);
+    println!("{}", repo_stars_badge);
 }
 
 #[cfg(test)]
